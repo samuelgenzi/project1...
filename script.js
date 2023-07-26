5064857ed553978454911b129e3ae9cfb0411d63
samuel <samuelhaileabg@gmail.com>
2023-07-26 15:41:02 +1000

    fetch the html we need

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=sydney&appid=3
 const apiKey = '3d3b7fc3ef1ccb996dfd9a99c3aeceda'; 
 
 
+const searchInput = document.getElementById('my-input');
+const searchButton = document.querySelector('.search button');
+const weatherIcon = document.querySelector('.weather-icon');
+const temperature = document.querySelector('.temp');
+const city = document.querySelector('.city');
+const windSpeed = document.querySelector('.wind');
+const humidity = document.querySelector('.humidity');
