02792249239d4e6bba9d5b15f5c6aef4ac34b39a
samuel <samuelhaileabg@gmail.com>
2023-07-26 15:39:23 +1000

    first fetch api

---
+++ script.js
@@ -0,0 +1,5 @@
+
+const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=sydney&appid=3d3b7fc3ef1ccb996dfd9a99c3aeceda&units=metric';
+const apiKey = '3d3b7fc3ef1ccb996dfd9a99c3aeceda'; 
+
+
